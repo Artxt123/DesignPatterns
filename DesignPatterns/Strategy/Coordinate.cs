a5772a202188b6829af272f7d4900b6d3febd98d
Artur Oczkowski <alfaix@o2.pl>
2021-10-25 23:12:49 +0200

    3 Strategia (strategy)

---
+++ DesignPatterns/Strategy/Coordinate.cs
@@ -0,0 +1,8 @@
+﻿namespace Strategy
+{
+    public class Coordinate
+    {
+        public double Long { get; set; }
+        public double Lat { get; set; } //latitude - szerokość geograficzna
+    }
+}
---
+++ DesignPatterns/Strategy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strategy = new WalkStrategy();
+
+            var map = new Map(strategy);
+
+            var start = new Coordinate();
+            var end = new Coordinate();
+
+            map.CreateRoute(start, end);
+        }
+    }
+}
