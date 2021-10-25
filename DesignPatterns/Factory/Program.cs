ff89381be836f905703aec847879efa0eec9b83c
Artur Oczkowski <alfaix@o2.pl>
2021-10-25 22:25:27 +0200

    2 Fabryka (factory)

---
+++ DesignPatterns/Factory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wzorzec Fabryka oddziela kod, który tworzy konkretne obiekty od kodu, który faktycznie z nich korzysta
+            //W ten sposób będzie nam o wiele łatwiej rozszerzać kod tworzący nowe obiekty bez konieczności ingerencji w resztę kodu
+            var shapeFactory = new ShapeFactory();
+
+            var circle = shapeFactory.CreateShape(ShapeType.Circle);
+            circle.Render();
+
+            var rectangle = shapeFactory.CreateShape(ShapeType.Rectangle);
+            rectangle.Render();
+
+            var triangle = shapeFactory.CreateShape(ShapeType.Triangle);
+            triangle.Render();
+        }
+    }
+}
