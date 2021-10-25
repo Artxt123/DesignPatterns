f9c6ee061b89cb44163f7c6f3c399bfa5da848ce
Artur Oczkowski <alfaix@o2.pl>
2021-10-25 23:49:06 +0200

    4 Adapter

---
+++ DesignPatterns/Adapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adapter umożliwia na współprace dwóch klas o niekompatybilnych interfejsach.
+            //W tym przykładzie SmsSender nie jest kompatybilny z interfejsem INotificationSender, dlatego tworzymy adapter, który implementuje ten interfejs i jednocześnie korzysta z klast SmsSender
+            INotificationSender notificationSender = new EmailSender();
+            notificationSender.SendNotification(1, new Notification { Title = "Tytuł", Body = "Ciało wiadomości" });
+
+            INotificationSender notificationSender2 = new SmsSenderAdapter();
+            notificationSender2.SendNotification(100, new Notification { Title = "Tytuł", Body = "Ciało wiadomości" });
+        }
+    }
+}
---
+++ DesignPatterns/Adapter/SmsSenderAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class SmsSenderAdapter : INotificationSender
+    {
+        private SmsSender _smsSender = new SmsSender();
+        public void SendNotification(int userId, Notification notification)
+        {
+            string userNumberPhone = userId.ToString();// ten numer telefonu trzeb awyciągnąć na podstawie userId
+            _smsSender.SendSms(userNumberPhone, $"Title: {notification.Title}\nBody: {notification.Body}");
+        }
+    }
+}
