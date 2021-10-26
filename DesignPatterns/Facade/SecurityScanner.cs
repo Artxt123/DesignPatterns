75cf99cddd784b24065014eaf40a9b84f93e01f6
Artur Oczkowski <alfaix@o2.pl>
2021-10-26 22:32:18 +0200

    5 Fasada (facade)

---
+++ DesignPatterns/Facade/SecurityScanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class SecurityScanner
+    {
+        public IEnumerable<string> SecurityScan(string hithubUrl)
+        {
+            Console.WriteLine("Security scan");
+            return new List<string>() { "security error1", "security error2" };
+        }
+    }
+}
